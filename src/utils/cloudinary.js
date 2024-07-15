1a2caadc4eec5613a44c349f25e795ced507817e
surajyadavdev <surajyadavrec55@gmail.com>
2024-07-15 13:02:39 +0530

    all setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs, { unlinkSync } from "fs";
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+// Upload an image
+const uploadOnCloudinary = async(localFilePath)=> {
+    try {
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // FILE HAS BEEN UPLOADED SUCCESSFULLY 
+        console.log("file has been uploaded on cloudinary successfully",response.url);
+        return response;
+    } catch (error) {
+        fs,unlinkSync(localFilePath)// remove the locally saved file as the upload operation got failed
+        return null;
+    }
+}
+    
+export {uploadOnCloudinary};    
+    
+    
+
+
+
